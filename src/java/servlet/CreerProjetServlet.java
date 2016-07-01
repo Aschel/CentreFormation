4f614300dcbaf9d2ba3720f8ab685ca26d9a138a
Utilisateur <Utilisateur@DESKTOP-MG5GV8P.home>
2016-07-01 17:12:40 +0200

    FormProjet V2

--- src/java/servlet/CreerProjetServlet.java
+++ src/java/servlet/CreerProjetServlet.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package servlet;
 
 import java.io.IOException;
@@ -32,7 +27,6 @@ public class CreerProjetServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-       request.getRequestDispatcher("WEB-INF/formprojet.jsp");
+        request.getRequestDispatcher("WEB-INF/formProjet.jsp").forward(request, response);
     }
 }
-   
