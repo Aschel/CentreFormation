79862e625942b74d8889c06818592160a03f5b9d
user <user@HP.home>
2016-07-01 15:28:15 +0200

    Embryon de projet

---
+++ src/java/modele/Projet.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modele;
+
+import java.sql.Date;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author user
+ */
+public class Projet {
+    Integer id;
+    
+
+    String sujet;
+    String titre;
+    Date dateCreation;
+    Date dateLimite;
+    
+    
+    public void insert() throws SQLException{
+            
+            }
+    
+    public static Projet getById(int id){
+        throw new UnsupportedOperationException("pas implémenté");
+    }
+}
