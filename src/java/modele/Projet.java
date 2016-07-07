f30e7a2f74165d2b9e4fc56a784792e50139413d
user <user@HP.home>
2016-07-07 12:32:21 +0200

    formProjet.jsp V1.0

--- src/java/modele/Projet.java
+++ src/java/modele/Projet.java
@@ -6,7 +6,7 @@
 package modele;
 
 import java.sql.Connection;
-import java.sql.Date;
+import java.util.Date;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -40,7 +40,7 @@ public class Projet {
         Projet result = null;
         Connection connection = Database.getConnection();
         Statement stmt = connection.createStatement();
-        ResultSet rs = stmt.executeQuery("SELECT * FROM produit WHERE id_projet=" + id);
+        ResultSet rs = stmt.executeQuery("SELECT * FROM projet WHERE id_projet=" + id);
         if (rs.next()) {
             result = new Projet(id, rs.getInt("id_promotion"), rs.getInt("id_createur"), rs.getString("sujet"), rs.getString("titre"), rs.getDate("date_creation"), rs.getDate("date_limite"));
         }
@@ -48,6 +48,10 @@ public class Projet {
         stmt.close();
         connection.close();
         return result;
+    }
+
+    Projet() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     public int getId() {
@@ -106,6 +110,7 @@ public class Projet {
         this.dateLimite = dateLimite;
     }
 
+    /*
     public void insert() throws SQLException {
         //assert nom != null && !nom.matches("/^ \t\n\r$");
         Connection connection = Database.getConnection();
@@ -135,6 +140,6 @@ public class Projet {
         } finally {
             connection.close();
         }
-
+*/
     }
 }
--- test/modele/ProjetTest.java
+++ test/modele/ProjetTest.java
@@ -1,5 +1,10 @@
 package modele;
 
+import java.util.Date;
+import java.sql.SQLException;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
@@ -8,24 +13,30 @@ import static org.junit.Assert.*;
  * @author Utilisateur
  */
 public class ProjetTest {
-    
+
     @Test
     public void testInsert() throws Exception {
         System.out.println("insert");
         Projet instance = new Projet();
-        instance.insert();
+        //instance.insert();
         assertEquals(4, instance.getId());
         Projet expected = Projet.getById(4);
         assertEquals(expected, instance);
     }
 
     @Test
-    public void testGetById() {
+    public void testGetById() throws SQLException, ParseException {
         System.out.println("getById");
         int id = 0;
-        Projet expected = new Projet(); //avec valeur du 1
+        String strDeb = "2016-06-11 00:00:00";
+        String strFin = "2017-07-01 00:00:00";
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+        Date dateDeb = formatter.parse(strDeb);
+        Date dateFin = formatter.parse(strFin);
+
+        Projet expected = new Projet(1, 1, 10, "Organisation du projet de creation web", "Organisation", dateDeb, dateFin); //avec valeur du 1
         Projet result = Projet.getById(1);
         assertEquals(expected, result);
     }
-    
+
 }
